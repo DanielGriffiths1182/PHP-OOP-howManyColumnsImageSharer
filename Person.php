d5cda0058ab8343d012e8b86937926e4fb78059c
Daniel Griffiths <Logiconictvtube@gmail.com>
2018-02-20 18:06:56 -0500

    Update Person.php

--- Person.php
+++ Person.php
@@ -5,15 +5,9 @@ class Person {
 
     public $name;
     public $surname;
-    public $age;
-    private $unava = 0;
 
     public function __construct($name) {
         $this->name = $name;
-    }
-
-    public function pozdrav(){
-        echo('Ahoj, já jsem ' . $this->$name);
     }
     
 }
